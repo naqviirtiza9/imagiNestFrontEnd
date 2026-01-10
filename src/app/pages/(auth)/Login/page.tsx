d0e64b0357d9a933ca6d432f11eee69ce0f91e74
Irtiza <naqviirtiza9@gmail.com>
2026-01-10 22:28:39 +0500

    Login page v1

--- src/app/pages/(auth)/Login/page.tsx
+++ src/app/pages/(auth)/Login/page.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const LoginPage = () => {
   return (
-    <div>
-      Login
+    <div className='flex justify-center items-center z-10 p-10 relative'>
+      <h1 className='flex justify-center items-center'>
+        Login
+      </h1>
     </div>
   )
 }
