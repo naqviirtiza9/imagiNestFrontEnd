695d4ab0838bc3f601d7bd2cf9d8eed3be2789b9
irtiza naqvi <naqviirtiza9@gmail.com>
2026-05-05 23:38:02 +0500

    Default Path updated

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/pages/Login', // This must match your actual URL path
+        permanent: true, 
+      },
+    ];
+  },
 };
 
 export default nextConfig;
