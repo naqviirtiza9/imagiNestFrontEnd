8ad677d442a1006af4acb398f1d03f6202a984a2
irtiza naqvi <naqviirtiza9@gmail.com>
2026-05-06 18:51:22 +0500

    Signup Page update

--- src/app/pages/(auth)/SignUp/page.tsx
+++ src/app/pages/(auth)/SignUp/page.tsx
@@ -13,6 +13,20 @@ const SignUpPage = () => {
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
           <form action="#" method="POST" className="space-y-6">
             <div>
+              <label className="block text-sm/6 font-medium text-gray-100">First Name</label>
+              <div className="mt-2">
+                <input id="text" type="text" name="fn" required  className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
+              </div>
+            </div>
+
+            <div>
+              <label className="block text-sm/6 font-medium text-gray-100">Last Name</label>
+              <div className="mt-2">
+                <input id="text" type="text" name="ln" required  className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
+              </div>
+            </div>
+
+            <div>
               <label className="block text-sm/6 font-medium text-gray-100">Email address</label>
               <div className="mt-2">
                 <input id="email" type="email" name="email" required  className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
